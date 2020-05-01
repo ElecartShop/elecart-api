4e6c27c2e7d6b011942bafc7c4b2936f9a5a5e29
Benjamin J. Young <blaher09@gmail.com>
2020-05-01 07:40:14 -0400

    OOPing schema

--- graphql/schema.js
+++ graphql/schema.js
@@ -10,125 +10,32 @@ const basename = path.basename(__filename);
 var queries = {};
 var mutations = {};
 
-function single(object, type, model) {
-  return {
-    name: object,
-    type: type,
-    args: {
-      id: {
-        type: new GraphQLNonNull(GraphQLString)
-      }
-    },
-    resolve(root, params) {
-      const data = model.findById(params.id).exec();
-      if (!data) {
-        throw new Error('Error');
-      }
-      return data;
-    }
-  };
-}
-
-function many(object, type, model) {
-  return {
-    name: object+'s',
-    type: new GraphQLList(type),
-    resolve() {
-      const data = model.find({deleted: null}).exec();
-      if (!data) {
-        throw new Error('Error');
-      }
-      return data;
-    }
-  };
-}
-
-function create(object, type, model) {
-  return {
-    name: object+'Create',
-    type: type,
-    args: {
-      name: {
-        type: new GraphQLNonNull(GraphQLString),
-      }
-    },
-    resolve(root, params) {
-      const entity = new model(params);
-      const data = entity.save();
-      if (!data) {
-        throw new Error('Error');
-      }
-      return data;
-    }
-  };
-}
-
-function update(object, type, model) {
-  return {
-    name: object+'Update',
-    type: type,
-    args: {
-      id: {
-        name: 'id',
-        type: new GraphQLNonNull(GraphQLString)
-      },
-      name: {
-        type: new GraphQLNonNull(GraphQLString),
-      }
-    },
-    resolve(root, params) {
-      return model.findByIdAndUpdate(
-        params.id,
-        { $set: {
-          name: params.name
-        } },
-        { new: true }
-      )
-        .catch(err => new Error(err));
-    }
-  };
-}
-
-function remove(object, type, model) {
-  return {
-    name: object+'Delete',
-    type: type,
-    args: {
-      id: {
-        name: 'id',
-        type: new GraphQLNonNull(GraphQLString)
-      }
-    },
-    resolve(root, params) {
-      return model.findByIdAndUpdate(
-        params.id,
-        { $set: { deleted: Date.now().toString() } },
-        { new: true }
-      )
-        .catch(err => new Error(err));
-    }
-  };
-}
-
 fs
   .readdirSync(__dirname+'/types')
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
+    var schemaClass;
+    if (fs.existsSync('schemas/'+file)) {
+      schemaClass = require('./schemas/'+file);
+    } else {
+      schemaClass = require('./schemas/index');
+    }
+
     const object = file.slice(0, -3);
     const type = require('./types/'+object);
     const model = require('../models/'+object);
     const objectType = new GraphQLObjectType(type);
 
-    queries[object] = single(object, objectType, model);
+    queries[object] = schemaClass.single(object, objectType, model);
     if (!type.noMany) {
-      queries[object+'s'] = many(object, objectType, model);
+      queries[object+'s'] = schemaClass.many(object, objectType, model);
     }
 
-    mutations[object+'Create'] = create(object, objectType, model);
-    mutations[object+'Update'] = update(object, objectType, model);
-    mutations[object+'Delete'] = remove(object, objectType, model);
+    mutations[object+'Create'] = schemaClass.create(object, objectType, model);
+    mutations[object+'Update'] = schemaClass.update(object, objectType, model);
+    mutations[object+'Delete'] = schemaClass.remove(object, objectType, model);
   });
 
 module.exports = {queries, mutations};
