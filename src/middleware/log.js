229fa96b93dcb6f851e3f3cbef3b9a3f7d20916c
Benjamin J. Young <blaher09@gmail.com>
2020-05-05 08:40:06 -0400

    Adding todo

--- src/middleware/log.js
+++ src/middleware/log.js
@@ -1,5 +1,6 @@
 const Call = require('../models/call').Model;
 
+// TODO: This doesn't actually track unless they send it through the variables
 module.exports = (req, res, next) => {
   if (req.body.variables && req.body.variables.shop_id) {
     var call = Call.create({shop_id: req.body.variables.shop_id});
